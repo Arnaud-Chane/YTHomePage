6ad010886f2326acc3b78f82f14d19b17f86ff9a
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-12-14 10:41:29 +0100

    create Button component

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Menu } from "lucide-react";
+import logo from "../assets/Logo.png";
 
 const PageHeader = () => {
   return (
-    <div>PageHeader</div>
-  )
-}
+    <div className="flex gap-10 lg:gap-20 justify-between">
+      <div className="flex gap-4 items-center flex-shrink-0">
+        <button>
+          <Menu />
+        </button>
+        <a href="/">
+          <img src={logo} alt="logo" className="h-6" />
+        </a>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
 
-export default PageHeader
+export default PageHeader;
