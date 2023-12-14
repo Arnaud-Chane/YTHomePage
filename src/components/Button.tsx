96a2b4c15bcb89f6fea9066784f0d759d1f3804b
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-12-14 11:22:48 +0100

    type props buttonprops via variantprops

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,28 @@
-import { cva } from "class-variance-authority";
+import { VariantProps, cva } from "class-variance-authority";
 
-const buttonStyles = cva(["hover:bg-secondaty-hover"])
+const buttonStyles = cva(["transition-colors"], {
+  variants: {
+    variant: {
+      default: ["bg-secondary", "hover:bg-secondaty-hover"],
+      ghost: ["hover:bg-gray-100"]
+    },
+    size: {
+      default: ["rounded", "p-2"],
+      icon: [
+        "rounded-full",
+        "w-10",
+        "h-10",
+        "flex",
+        "items-center",
+        "justify-center",
+        "p-2.5",
+      ],
+    },
+  },
+});
 
-const Button = () => {
-  return <button></button>;
-};
+type ButtonProps = VariantProps<typeof buttonStyles>
 
-export default Button;
+export function Button ({ variant, size }: ButtonProps) {
+  return <button className={buttonStyles({ variant, size })} />
+}
