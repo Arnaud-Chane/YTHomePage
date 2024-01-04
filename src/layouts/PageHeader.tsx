f6cbff8c083f77e26967307dae953670189002e0
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2024-01-04 10:58:14 +0100

    Adding right button to header

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,22 +1,30 @@
-import { Menu } from "lucide-react";
+import { Bell, Menu, Upload, User } from "lucide-react";
 import logo from "../assets/Logo.png";
 import { Button } from "../components/Button";
 
-const PageHeader = () => {
-  return (
-    <div className="flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4">
-      <div className="flex gap-4 items-center flex-shrink-0">
-        <Button variant="ghost" size="icon">
-          <Menu />
-        </Button>
-        <a href="/">
-          <img src={logo} alt="logo" className="h-6" />
-        </a>
-      </div>
-      <div></div>
-      <div></div>
+const PageHeader = () => (
+  <div className="flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4">
+    <div className="flex gap-4 items-center flex-shrink-0">
+      <Button variant="ghost" size="icon">
+        <Menu />
+      </Button>
+      <a href="/">
+        <img src={logo} alt="logo" className="h-6" />
+      </a>
     </div>
-  );
-};
+    <div></div>
+    <div className="flex flex-shrink-0 md:gap-2">
+      <Button size="icon" variant="ghost">
+        <Upload />
+      </Button>
+      <Button size="icon" variant="ghost">
+        <Bell />
+      </Button>
+      <Button size="icon" variant="ghost">
+        <User />
+      </Button>
+    </div>
+  </div>
+);
 
 export default PageHeader;
